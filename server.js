af7bc1b5eab3ebd89c636082c5b74e67afee6562
Richard Wilbur <richard.wilbur@hotmail.com>
2017-06-09 21:16:30 -0700

    Add disconnect event and message

--- server.js
+++ server.js
@@ -12,9 +12,26 @@ var clientInfo = {};
 io.on('connection', socket => {
 	console.log('User connected via socket.io');
 
+	socket.on('disconnect', () => {
+		var userData = clientInfo[socket.id];
+
+		if (typeof userData !== 'undefined') {
+			// Use socket.leave feature to leave a room
+			socket.leave(userData.room);
+
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				timestamp: moment().valueOf(),
+				text: userData.name + ' has left!'
+			});
+
+			delete clientInfo[socket.id];
+		}
+	});
+
 	socket.on('joinRoom', request => {
 		// Store the room name and other info using socket.Id as the key
-		clientInfo[socket.Id] = request;
+		clientInfo[socket.id] = request;
 
 		// Use socket.io room feature to add/join a room
 		socket.join(request.room);
@@ -36,7 +53,7 @@ io.on('connection', socket => {
 		message.timestamp = moment().valueOf(),
 		
 		// Send message to all:
-		io.to(clientInfo[socket.Id].room).emit('message', message);
+		io.to(clientInfo[socket.id].room).emit('message', message);
 	});
 
 	socket.emit('message', {
